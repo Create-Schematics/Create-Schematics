b154194431f7d31779313cc9e7f224c3b3c2882b
Charlie Wood <rabbitminers2.0@gmail.com>
2023-01-14 21:36:52 +0000

    Added link data

---
+++ src/data/links.ts
@@ -0,0 +1,25 @@
+interface LinkData {
+    image: string;
+    name: string;
+    link: string;
+}
+
+const LINKS: LinkData[] = [
+    {
+        image: "/home/reddit.webp",
+        name: "r/CreateSchematics",
+        link: ""
+    },
+    {
+        image: "/home/discord.webp",
+        name: "Addon Hub",
+        link: ""
+    },
+    {
+        image: "/home/curseforge.webp",
+        name: "Companion Mod",
+        link: ""
+    }
+]
+
+export default LINKS;
