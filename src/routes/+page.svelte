0f4d30cde841c9ae2c4207aea7fd2ab045581a76
Charlie Wood <rabbitminers2.0@gmail.com>
2023-01-14 17:01:13 +0000

    Implemented homescreen scene

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { createScene } from '../lib/scene';
+	import { createScene } from '../lib/homeScene';
 	let el: any;
 	onMount(() => {
 		createScene(el);
@@ -13,4 +13,27 @@
 	<meta name="description" content="Three.js example app built with Svelte" />
 </svelte:head>
 
+<div class='title'>
+	Create: <br/>Schematics
+	<hr/>
+</div>
+
+<div class='links'>
+	
+</div>
+
 <canvas bind:this={el} />
+
+<style lang="scss">
+	.title {
+		margin: 20px;
+		font-size: 3.5em;
+		position: absolute;
+
+		> hr {
+			margin: 0;
+			margin-top: 10px;
+			width: 80%;
+		}
+	}
+</style>
