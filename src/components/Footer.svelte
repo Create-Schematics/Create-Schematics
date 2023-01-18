260faf8fe3bbf763aca458594b731f4bb46e8757
Charlie Wood <rabbitminers2.0@gmail.com>
2023-01-18 17:06:44 +0000

    Created Footer Component

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,39 @@
 <script>
-
+    import FOOTER from "../data/footer_data";
 </script>
 
-<div>
-
+<div class="footer">
+    {#each FOOTER as footerColumn}
+        <div class="footer-column">
+            <h3>{footerColumn.heading}</h3> <hr/>
+            {@html footerColumn.body}
+        </div>
+    {/each}
 </div>
 
-<style>
-    
+<style lang="scss">
+
+    .footer {
+        font-size: large;
+        display: flex;
+        flex-direction: row;
+
+        > div {
+            margin: 20px;
+            width: 33%;
+        }
+    }
+
+    @media only screen and (max-width: 800px) { .footer {
+        font-size: large;
+        display: flex;
+        flex-direction: column;
+
+        > div {
+            width: 100%;
+            border-radius: 20px;
+            margin: 20px;
+        }
+    }}
+
 </style>
