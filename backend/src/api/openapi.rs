93b582242867046bb51f8ea1aa907509941b6f71
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-11-20 16:36:22 +0000

    Implemented and documented favorite schematics

--- backend/src/api/openapi.rs
+++ backend/src/api/openapi.rs
@@ -1,6 +1,18 @@
-use utoipa::OpenApi;
+use utoipa::{OpenApi, Modify};
+
+use crate::authentication::session::Session;
 
 use super::v1;
+
+struct AuthenticationModifier;
+
+impl Modify for AuthenticationModifier {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(components) = openapi.components.as_mut() {
+            components.add_security_scheme("session_cookie",  Session::security_scheme())
+        }
+    }
+}
 
 #[derive(OpenApi)]
 #[openapi(
@@ -10,6 +22,10 @@ use super::v1;
         license(name = "MIT", url = "https://github.com/Rabbitminers/Create-Schematics/blob/master/LICENSE")
     ),
     paths(
+        v1::favorites::get_favorites,
+        v1::favorites::favorite_schematic,
+        v1::favorites::unfavorite_schematic,
+
         v1::schematics::search_schematics,
         v1::schematics::upload_schematic,
         v1::schematics::get_schematic_by_id,
@@ -20,6 +36,9 @@ use super::v1;
         v1::users::signup,
         v1::users::login,
         v1::users::logout,
+    ),
+    modifiers(
+        &AuthenticationModifier
     ),
     components(schemas(
         crate::models::user::User,
---
+++ backend/src/api/v1/favorites.rs
@@ -0,0 +1,131 @@
+use axum::Router;
+use axum::extract::{State, Path};
+use axum::routing::{post, get};
+use axum::Json;
+
+use crate::api::ApiContext; 
+use crate::authentication::session::Session;
+use crate::response::ApiResult;
+use crate::models::schematic::Schematic;
+
+pub (in crate::api::v1) fn configure() -> Router<ApiContext> {
+    Router::new()
+        .route(
+            "/schematics/:id", 
+            get(get_favorites)
+        )
+        .route(
+            "/schematics/:id/favorite", 
+        post(favorite_schematic)
+            .delete(unfavorite_schematic)
+        )
+}
+
+#[utoipa::path(
+    get,
+    path = "/schematics/favorites",
+    context_path = "/api/v1",
+    tag = "v1",
+    responses(
+        (status = 200, description = "Successfully retrieved the schematics", body = [Schematic], content_type = "application/json"),
+        (status = 401, description = "You need to be logged in to view your favourite schematics"),
+        (status = 500, description = "An internal server error occurred")
+    ),
+    security(("session_cookie" = []))
+)]
+async fn get_favorites(
+    State(ctx): State<ApiContext>,
+    session: Session,
+) -> ApiResult<Json<Vec<Schematic>>> {
+    let schematics = sqlx::query_as!(
+        Schematic,
+        r#"
+        select schematic_id, schematic_name,
+               game_version, create_version, 
+               downloads, author
+        from favorites
+        inner join schematics
+        using (schematic_id)
+        where user_id = $1
+        "#,
+        session.user_id
+    )
+    .fetch_all(&ctx.pool)
+    .await?;
+
+    Ok(Json(schematics))
+}
+
+#[utoipa::path(
+    post,
+    path = "/schematics/{id}/favorite",
+    context_path = "/api/v1",
+    tag = "v1",
+    params(
+        ("id" = String, Path, description = "The id of the schematic to favorite")
+    ),
+    responses(
+        (status = 200, description = "Successfully favorited the schematic"),
+        (status = 401, description = "You need to be logged in to favorite a schematic"),
+        (status = 500, description = "An internal server error occurred")
+    ),
+    security(("session_cookie" = []))
+)]
+async fn favorite_schematic(
+    State(ctx): State<ApiContext>,
+    session: Session,
+    Path(id): Path<i64>,
+) -> ApiResult<()> {
+    sqlx::query!(
+        r#"
+        insert into favorites (
+            schematic_id, user_id
+        )
+        values (
+            $1, $2
+        )
+        on conflict do nothing
+        "#,
+        id,
+        session.user_id
+    )
+    .execute(&ctx.pool)
+    .await?;
+
+    Ok(())
+}
+
+#[utoipa::path(
+    delete,
+    path = "/schematics/{id}/favorite",
+    context_path = "/api/v1",
+    tag = "v1",
+    params(
+        ("id" = String, Path, description = "The id of the schematic to unfavorite")
+    ),
+    responses(
+        (status = 200, description = "Successfully unfavorited the schematic"),
+        (status = 401, description = "You need to be logged in to unfavorite a schematic"),
+        (status = 500, description = "An internal server error occurred")
+    ),
+    security(("session_cookie" = []))
+)]
+async fn unfavorite_schematic(
+    State(ctx): State<ApiContext>,
+    session: Session,
+    Path(id): Path<i64>,
+) -> ApiResult<()> {
+    sqlx::query!(
+        r#"
+        delete from favorites
+        where schematic_id = $1
+        and user_id = $2
+        "#,
+        id,
+        session.user_id
+    )
+    .execute(&ctx.pool)
+    .await?;
+
+    Ok(())
+}
