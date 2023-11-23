06eab6a3d6b5cf4dd4e8c36c7dde0278e2c6f737
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-11-23 22:22:18 +0000

    Added permssion bit flags

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -4,8 +4,36 @@ use uuid::Uuid;
 #[derive(Debug, Serialize, ToSchema)]
 pub struct User {
     pub user_id: Uuid,
+    
     pub username: String,
+
     pub email: String,
+
     #[serde(skip_serializing)]
     pub password_hash: String
+}
+
+bitflags::bitflags! {
+    #[derive(Serialize, Deserialize, ToSchema)]
+    #[serde(transparent)]
+    pub struct Permissions: u64 {
+        const VOTE = 1 << 0;
+        const COMMENT = 1 << 1;
+        const POST = 1 << 2;
+        const MODERATE_COMMENTS = 1 << 3;
+        const MODERATE_POSTS = 1 << 4;
+        const MODERATE_USERS = 1 << 5;
+    }
+}   
+
+impl From<i64> for Permissions {
+    fn from(value: i64) -> Self {
+        Permissions::from_bits(value as u64).unwrap_or_default()
+    }
+}
+
+impl Default for Permissions {
+    fn default() -> Self {
+        Permissions::VOTE | Permissions::COMMENT | Permissions::POST
+    }
 }
