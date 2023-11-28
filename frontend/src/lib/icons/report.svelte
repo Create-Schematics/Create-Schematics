fc527e1737d767fc88da0f5b2414523613c1a710
levtus <i@alexander.systems>
2023-11-28 10:12:59 -0600

    Testing pixelated icons

---
+++ frontend/src/lib/icons/report.svelte
@@ -0,0 +1,10 @@
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+ width="12pt" height="12pt" viewBox="0 0 512.000000 512.000000"
+ preserveAspectRatio="xMidYMid meet" class="inline fill-inherit">
+
+<g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" stroke="none">
+<path d="M640 2560 l0 -2560 320 0 320 0 0 1120 0 1120 640 0 640 0 0 -160 0
+-160 160 0 160 0 0 -160 0 -160 960 0 960 0 0 1440 0 1440 -800 0 -800 0 0
+160 0 160 -160 0 -160 0 0 160 0 160 -1120 0 -1120 0 0 -2560z"/>
+</g>
+</svg>
