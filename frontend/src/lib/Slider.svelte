2998460d557148484ea4e8899a188dadf1103217
Szedann <65607498+Szedann@users.noreply.github.com>
2023-11-28 18:46:41 +0100

    simplify the slider

--- frontend/src/lib/Slider.svelte
+++ frontend/src/lib/Slider.svelte
@@ -1,28 +1,18 @@
 <script lang="ts">
   export let images: string[] = [];
-  let x = 0;
-  let currentImage = Math.round(x/images.length);
+  let currentImage = 0;
   let e: HTMLDivElement;
-  const updateScroll = () => {
-    x = e.scrollLeft
-    currentImage = Math.round(x/e.clientWidth);
-  }
-  const scrollToImage = (i: number) => {
-    e.scrollTo({left: i*e.clientWidth, behavior: "smooth"})
-  }
 </script>
 
-<div class="flex relative pixel-corners aspect-video overflow-hidden bg-create-blue/30">
-  <div class="overflow-x-scroll scroll-smooth snap-x snap-mandatory flex w-full" bind:this={e} on:scroll={updateScroll} >
-    {#each images as image}
-      <img src={image} alt="example" class="w-full snap-center" />
-    {/each}
+<div class="flex relative pixel-corners aspect-video overflow-hidden bg-create-blue/30 bg-checker">
+  <div class="overflow-x-scroll scroll-smooth snap-x snap-mandatory flex w-full" bind:this={e} >
+      <img src={images[currentImage]} alt="example" class="h-full w-full snap-center object-cover" />
   </div>
   <div
     class="bg-black text-white absolute bottom-2 px-2 left-1/2 -translate-x-1/2 pixel-corners flex gap-1 text-opacity-50"
   >
     {#each images as _, i}
-      <input type="button" class={`outline-none ${i==currentImage?"text-white":""}`} on:click={e=>scrollToImage(i)} value="■"/>
+      <input type="button" class={`outline-none ${i==currentImage?"text-white":""}`} on:click={()=>currentImage = i} value="■"/>
     {/each}
   </div>
 </div>
