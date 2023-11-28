b5c33a73cdf1e2b63023d1125da0d3d274285248
Szedann <65607498+Szedann@users.noreply.github.com>
2023-11-28 15:41:15 +0100

    Make a schematic card component

---
+++ frontend/src/lib/SchematicCard.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import type { Schematic } from "$lib";
+
+
+    export const {tags, uploadDate, title, author}: Schematic = {
+        tags: ["farm", "Above & Beyond", "trains", "equipment", "steampunk", "novel"],
+        uploadDate: new Date(1701111471000),
+        title: "Very cool schematic",
+        img: "https://picsum.photos/500/800",
+        downloads: 603,
+        likes: 60,
+        dislikes: 40,
+        views: 894,
+        author: "Szedann"
+    }
+</script>
+
+<div class=" mx-auto px-4 py-3 bg-minecraft-ui-light dark:bg-minecraft-ui-dark pixel-corners w-full">
+    <h1 class="text-xl font-bold">{title}</h1>
+    <div class="text-xs text-opacity-50 w-full">
+        <p class="inline"><b>⭳</b> 5,152</p>
+        <p class="inline">by <a href={`/user/${author}`} class="underline">{author}</a></p>
+        <p class="inline px-2">|</p>
+        <p class="inline text-right">{uploadDate.toLocaleString("en-US", { timeZone: 'UTC' })} UTC</p>
+        <div class="w-full py-3 aspect-video">
+            <img src="https://picsum.photos/500/800" alt="" class="object-cover pixel-corners w-full h-full overflow-hidden"/>
+        </div>
+    </div>
+
+    <div class="overflow-x-scroll whitespace-nowrap scrollbar-x scrollbar-small">
+        <ul class="flex flex-nowrap gap-2 mb-3">
+            {#each tags as tag}
+                <li class="text-xs bg-create-blue/80 dark:bg-create-blue/20 px-1 text-opacity-50">
+                    {tag}
+                </li>
+            {/each}
+        </ul>
+    </div>  
+</div>
