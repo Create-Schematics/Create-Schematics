64e20dee8c34b7c7d97a93c337f7b3837cc81f37
levtus <i@alexander.systems>
2023-11-27 23:07:54 -0600

    Erorr page, opt-out of default link styling class (.no-default-link)

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<main class="flex items-center justify-center w-screen" style="height: 90vh; padding-bottom: 15vh;">
+    <div class="text-center text-create-blue/800 bg-checker pixel-corners pt-20 pb-12 w-[calc(100vw-2rem)] max-w-6xl" 
+         style="text-shadow: 1px 1px 2px #101015, 0 0 1em #B3BDD8, 0 0 0.2em #B3BDD8;">
+        <h2 style="font-size: 4rem; line-height: 1;">ERROR</h2>
+        <h1 style="font-size: 10rem; line-height: 1;">404</h1>
+        <p class="text-4xl">Page not found</p>
+        <a class="no-default-link" href="../">
+            <button class="bg-create-blue/50 hover:bg-create-blue/80 cursor-pointer pixel-corners text-4xl px-5 py-2 mt-10">Home</button>
+        </a>
+    </div>
+</main>
