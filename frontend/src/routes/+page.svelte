fcd33dc7b5476fe898a7058c56a926a92ae3649d
levtus <i@alexander.systems>
2023-11-28 11:35:17 -0600

    First steps towards a landing page

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<main>
+    <div class="w-[calc(100vw-2rem)] h-[calc(100vh-6rem)] max-w-6xl p-4 mx-auto mt-3
+    text-center">
+        <h2 class="text-center text-4xl md:text-6xl w-full font-bold mt-40 leading-tight" style="text-shadow: 1px 1px 2px #B3BDD8, 0 0 1em #101015, 0 0 0.2em #101015;">The place for<br>Create Schematics</h2>
+        <h3 class="text-center text-2xl md:text-3xl w-full mt-10" style="text-shadow: 1px 1px 2px #B3BDD8, 0 0 1em #101015, 0 0 0.2em #101015;">Browse, download and share<br>schematics like never before.</h3>
+    </div>
+</main>
