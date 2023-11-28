b67cd934fcaa352b2f9d5bfe4794e555d2103193
Szedann <65607498+Szedann@users.noreply.github.com>
2023-11-28 15:41:29 +0100

    create a schematic interface

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,1 +1,11 @@
-//
+export interface Schematic {
+    tags: string[];
+    uploadDate: Date;
+    title: string;
+    img: string;
+    downloads: number;
+    likes: number;
+    dislikes: number;
+    views: number;
+    author: string;
+}
