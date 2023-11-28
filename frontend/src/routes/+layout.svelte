e65ddc35bd851625d7de1b525dee8f47812edc40
Szedann <65607498+Szedann@users.noreply.github.com>
2023-11-28 20:36:45 +0100

    update layout stuff

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <body
-  class=" bg-create-blue/50 dark:bg-gray-800 dark:text-white h-screen absolute w-full flex flex-col gap-3 overflow-x-hidden font-pixel"
+  class=" bg-create-blue/40 dark:bg-gray-800 dark:text-white h-screen absolute w-full flex flex-col gap-3 overflow-x-hidden font-pixel"
 >
   <header
     class="bg-minecraft-ui-light dark:bg-minecraft-ui-dark max-w-6xl p-4 mx-auto mt-3
@@ -11,7 +11,7 @@
   >
     <div class="flex items-center gap-3">
       <a href="/">
-        <img src="/favicon.png" class="w-10" alt="">
+        <img src="/favicon.png" class="w-10" alt="" />
       </a>
 
       <search title="schematics">
@@ -33,9 +33,7 @@
       >
         +
       </div>
-      <div
-        class="w-10 h-10 bg-white/50 overflow-hidden pixel-corners"
-      >
+      <div class="w-10 h-10 bg-white/50 overflow-hidden pixel-corners">
         <img src="https://picsum.photos/50" alt="" />
       </div>
     </div>
@@ -43,16 +41,20 @@
   <main class="flex-grow">
     <slot />
   </main>
-  <footer class=" bg-minecraft-ui-light dark:bg-minecraft-ui-dark opacity-75 p-4 justify-between items-center">
-
-    <div class="container mx-auto text-center md:text-left p-4 opacity-100 ">
+  <footer
+    class=" bg-minecraft-ui-light dark:bg-minecraft-ui-dark opacity-75 p-4 justify-between items-center"
+  >
+    <div class="container mx-auto text-center md:text-left p-4 opacity-100">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-xl mx-auto">
         <div class="mb-4 md:mb-0 items-center">
           <div class="w-fit mx-auto">
             <h2 class="text-lg font-semibold mb-2">Create Schematics</h2>
             <p class="text-m">
-              We're <a href="https://github.com/Create-Schematics/Create-Schematics">open source</a><br>
-              <a href="https://discord.gg/GJsQadv9Mc">Discord</a><br>
+              We're <a
+                href="https://github.com/Create-Schematics/Create-Schematics"
+                >open source</a
+              ><br />
+              <a href="https://discord.gg/GJsQadv9Mc">Discord</a><br />
               <a href="mailto:contact@createschematics.com">Email</a>
             </p>
           </div>
@@ -62,8 +64,8 @@
           <div class="w-fit mx-auto">
             <h2 class="text-lg font-semibold mb-2">Resources</h2>
             <p class="text-m">
-              <a href="../terms">Terms of Use</a><br>
-              <a href="../privacy">Privacy</a><br>
+              <a href="../terms">Terms of Use</a><br />
+              <a href="../privacy">Privacy</a><br />
               <a href="../rules">Upload Rules</a>
             </p>
           </div>
@@ -71,9 +73,19 @@
       </div>
 
       <div class="mx-auto mt-8 text-sm text-gray-500 text-center opacity-100">
-        <p>&copy; 2023. <a class="text-slate-500 hover:text-slate-400" href="https://github.com/Create-Schematics/Create-Schematics/blob/master/LICENSE">Licensed under the MIT License</a>.</p>
+        <p>
+          &copy; 2023. <a
+            class="text-slate-500 hover:text-slate-400"
+            href="https://github.com/Create-Schematics/Create-Schematics/blob/master/LICENSE"
+            >Licensed under the MIT License</a
+          >.
+        </p>
 
-        <p>NOT AN OFFICIAL MINECRAFT PRODUCT. NOT APPROVED BY OR ASSOCIATED WITH MOJANG.<br>NOT APPROVED BY OR ASSOCIATED WITH THE CREATE MOD.</p>
+        <p>
+          NOT AN OFFICIAL MINECRAFT PRODUCT. NOT APPROVED BY OR ASSOCIATED WITH
+          MOJANG.<br />NOT APPROVED BY OR ASSOCIATED WITH THE CREATE MOD.
+        </p>
       </div>
+    </div>
   </footer>
 </body>
--- frontend/src/routes/user/[username]/+page.svelte
+++ frontend/src/routes/user/[username]/+page.svelte
@@ -43,6 +43,9 @@
       id: "1",
     },
   ];
+  schematics.push(...schematics);
+  schematics.push(...schematics);
+  schematics.push(...schematics);
 </script>
 
 <svelte:head>
@@ -55,32 +58,39 @@ w-[calc(100vw-2rem)] justify-between items-left pixel-corners"
 >
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-left">
     <div
-      class="bg-minecraft-ui-light dark:bg-minecraft-ui-dark pixel-corners w-full p-3 flex gap-1 flex-col"
+      class="pixel-corners p-3 bg-minecraft-ui-light dark:bg-minecraft-ui-dark"
     >
-      <div class="flex gap-2">
-        <img
-          src="https://picsum.photos/500"
-          alt=""
-          class="w-24 h-24 pixel-corners"
-        />
-        <div>
-          <h2 class="text-2xl font-bold">Username123</h2>
-          <div class="text-opacity-50 text-black dark:text-white">
-            <p>
-              Joined <b>{intlFormatDistance(user.dateJoined, Date.now())}</b>
-            </p>
-            <p><b>15</b> Submissions</p>
-            <p><b>24k</b> Downloads</p>
+      <div
+        style="--checker-size: 72px; --checker-color: #fff1;"
+        class="bg-create-blue pixel-corners w-full h-full p-3 flex gap-1 flex-col bg-checker"
+      >
+        <div class="flex gap-2">
+          <img
+            src="https://picsum.photos/500"
+            alt=""
+            class="w-24 h-24 pixel-corners"
+          />
+          <div>
+            <h2 class="text-2xl font-bold">{user.username}</h2>
+            <div>
+              <p>
+                Joined <b>{intlFormatDistance(user.dateJoined, Date.now())}</b>
+              </p>
+              <p><b>15</b> Submissions</p>
+              <p><b>{user.totalDownloads}</b> Downloads</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="flex flex-col gap-1">
-        <div class="flex gap-2 flex-wrap">
-          {#each user.links as link}
-            <a href={link.url}>{link.name}</a>
-          {/each}
+        <div class="flex flex-col gap-1">
+          <div class="flex gap-2 flex-wrap">
+            {#each user.links as link}
+              <a class="bg-black dark:bg-white px-2" href={link.url}
+                >{link.name}</a
+              >
+            {/each}
+          </div>
+          {user.description}
         </div>
-        {user.description}
       </div>
     </div>
     {#each schematics as schematic}
