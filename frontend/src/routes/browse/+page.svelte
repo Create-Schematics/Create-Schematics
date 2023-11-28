855cb2faae70066cfd84c0d1c831615ab18783c4
Szedann <65607498+Szedann@users.noreply.github.com>
2023-11-28 16:23:50 +0100

    remove padding in the browse route

--- frontend/src/routes/browse/+page.svelte
+++ frontend/src/routes/browse/+page.svelte
@@ -17,7 +17,7 @@
     ]
 </script>
 
-<main class="max-w-6xl p-4 mx-auto mt-3
+<main class="max-w-6xl mx-auto mt-3
 w-[calc(100vw-2rem)] justify-between items-left pixel-corners">
 <h2 class="text-2xl text-white/500 p-1">Browse Schematics</h2>
 <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mx-auto items-left">
