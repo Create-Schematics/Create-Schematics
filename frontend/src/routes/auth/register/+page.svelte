25b432eac5b5eccf19b5789e9bc75ffeb5629975
levtus <i@alexander.systems>
2023-11-30 13:54:27 -0600

    Add discord, github oauth buttons. Improve light mode styles.

--- frontend/src/routes/auth/register/+page.svelte
+++ frontend/src/routes/auth/register/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import GoogleIcon from "$lib/icons/google.svelte"
     import MicrosoftIcon from "$lib/icons/microsoft.svelte";
+    import GithubIcon from "$lib/icons/github.svelte"
+    import DiscordIcon from "$lib/icons/discord.svelte";
 
     /* let usernameTakenError = false;
     let emailFormatError = false;
@@ -45,8 +47,18 @@
         <div class="mx-auto w-fit p-4 pixel-corners bg-minecraft-ui-light dark:bg-minecraft-ui-dark">
             <h2 class="text-xl font-bold text-center">Register with</h2>
             <div class="grid grid-cols-2 gap-4 px-3 py-4">
-                <button class="bg-create-blue/50 hover:bg-create-blue/80 w-full h-10 outline-none px-3 accent-create-blue pixel-corners text-xl whitespace-nowrap"><GoogleIcon/> Google</button>
-                <button class="bg-create-blue/50 hover:bg-create-blue/80 w-full h-10 outline-none px-3 accent-create-blue pixel-corners text-xl whitespace-nowrap"><MicrosoftIcon/> Microsoft</button>
+                <button class="dark:bg-create-blue/50 bg-white/50 hover:bg-white/30 dark:hover:bg-create-blue/80 w-full h-10 outline-none px-3 accent-create-blue pixel-corners text-xl whitespace-nowrap">
+                    <GoogleIcon/> Google
+                </button>
+                <button class="dark:bg-create-blue/50 bg-white/50 hover:bg-white/30 dark:hover:bg-create-blue/80 w-full h-10 outline-none px-3 accent-create-blue pixel-corners text-xl whitespace-nowrap">
+                    <MicrosoftIcon/> Microsoft
+                </button>
+                <button class="fill-black dark:fill-white bg-white/50 hover:bg-white/30 dark:bg-create-blue/50 dark:hover:bg-create-blue/80 w-full h-10 outline-none px-3 accent-create-blue pixel-corners text-xl whitespace-nowrap">
+                    <GithubIcon /> GitHub
+                </button>
+                <button class="fill-[#5865F2] dark:fill-white bg-white/50 hover:bg-white/30 dark:bg-create-blue/50 dark:hover:bg-create-blue/80 w-full h-10 outline-none px-3 accent-create-blue pixel-corners text-xl whitespace-nowrap">
+                    <DiscordIcon/> Discord
+                </button>
             </div>
             <!--<hr class="my-3 border-slate-800 mx-3">
             <h2 class="text-xl font-bold text-center p-3">Or do it manually</h2>
