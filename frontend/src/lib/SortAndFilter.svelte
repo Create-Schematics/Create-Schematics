b4d7127cc6b91d2f495bf54a75c478431a39ad86
levtus <i@alexander.systems>
2023-12-06 11:04:56 -0600

    Further work on collections page. Not entirely sure what design direction this should take.

---
+++ frontend/src/lib/SortAndFilter.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { Schematic } from "./types";
+
+    let searchTerm = "";
+    let selectedCategory = "";
+  
+    let items: Schematic[];
+    function filterItems() {
+      return items.filter((item) => {
+        // const matchesSearchTerm = item.name.toLowerCase().includes(searchTerm.toLowerCase());
+        // const matchesCategory = selectedCategory === "" || item.category === selectedCategory;
+        // return matchesSearchTerm && matchesCategory;
+      });
+    }
+  
+    function sortItems() {
+      // return filterItems().sort((a, b) => a.name.localeCompare(b.name));
+    }
+  </script>
+  
+  <div class="text-black">
+    <input type="text" id="search" bind:value={searchTerm} />
+
+    <select id="sort" bind:value={selectedCategory}>
+        <option value="default" selected>Alphabetical</option>
+        <option value="alphabetical">Alphabetical</option>
+        <option value="new">New</option>
+        <option value="old">Old</option>
+        <option value="views">Views</option>
+        <option value="downloads">Downloads</option>
+        <option value="size">Size</option>
+    </select>
+  </div>
--- frontend/src/routes/user/[username]/collection/[id]/+page.svelte
+++ frontend/src/routes/user/[username]/collection/[id]/+page.svelte
@@ -3,16 +3,33 @@
   import type { Schematic, Collection } from "$lib/types";
   import { getCollection, getSchematic } from "$lib/requests";
   import SchematicCard from "$lib/SchematicCard.svelte";
+  import SortAndFilter from "$lib/SortAndFilter.svelte";
   import { abbreviateNumber } from "../../../../../utils";
 
-  let collectionId: string = '';
+  let collectionId = '';
+
+  const collection = {
+      id: "1",
+      title: "Train Stations",
+      author: {
+        id: "1",
+        username: "sudolev",
+        avatar: "https://picsum.photos/500/800",
+      },
+      creation_date: new Date(1601111471000),
+      thumbnail_url: "https://picsum.photos/500/800",
+      schematics: [
+        {
+          id: "1",
+          title: "Very cool schematic",
+        }
+      ]
+    };
 
   if (typeof window !== 'undefined') {
     let collectionIdRaw: string = (window.location.pathname.replace(/\/$/, '')?.split('/')?.pop()?.split(/[?#]/)[0])!;
     collectionId = collectionIdRaw
   }
-
-  const collection: Collection = getCollection(collectionId);
 
   const schematics: Schematic[] = [
     {
@@ -48,35 +65,13 @@
   class="max-w-6xl mx-auto mt-3ss
 w-[calc(100vw-2rem)] justify-between items-left pixel-corners"
 >
+  <div class=" my-4 px-4 py-3 bg-minecraft-ui-light dark:bg-minecraft-ui-dark pixel-corners">
+    <h1 class="text-3xl inline">{collection.title}</h1>
+    <p class="hidden md:inline text-right float-right">{collection.schematics.length} Schematic{collection.schematics.length > 1 ? 's':''}</p>
+    <h2 class="text-xl ml-1 mt-2"><img class="w-8 h-8 pixel-corners inline" src="{collection.author.avatar}" alt="Avatar"> {collection.author.username}</h2>
+
+  </div>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-left">
-    <div
-      class="pixel-corners p-3 bg-minecraft-ui-light dark:bg-minecraft-ui-dark"
-    >
-      <div
-        style="--checker-size: 72px; --checker-color: #fff1;"
-        class="bg-create-blue pixel-corners w-full h-full p-3 flex gap-1 flex-col bg-checker"
-      >
-        <div class="flex gap-2">
-          <img
-            src="https://picsum.photos/500"
-            alt=""
-            class="w-24 h-24 pixel-corners"
-          />
-          <div>
-            <h2 class="text-2xl font-bold"></h2>
-            <div>
-              <p>
-              </p>
-              <p><b>15</b> Submissions</p>
-            </div>
-          </div>
-        </div>
-        <div class="flex flex-col gap-1">
-          <div class="flex gap-2 flex-wrap">
-          </div>
-        </div>
-      </div>
-    </div>
     {#each schematics as schematic}
       <SchematicCard {...schematic} />
     {/each}
