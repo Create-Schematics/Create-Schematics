517f534bdb5e335ee8b91776bd7f04f9be4df0b9
levtus <i@alexander.systems>
2023-12-07 14:31:57 -0600

    'Edit account details' page

--- frontend/src/routes/account/+page.svelte
+++ frontend/src/routes/account/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
     let username: string
-    let description: string
+    let tagline: string
+    let links: {name: string, url: string}[] = [
+        {
+            name: "",
+            url: ""
+        },
+        {
+            name: "",
+            url: ""
+        }
+    ]
 
     function saveChanges() {
     }
@@ -10,13 +20,35 @@
     <title>Account - Create Schematics</title>
 </svelte:head>
 
-<main class="flex items-center justify-center w-screen pt-2 pb-28">
-    <div class="bg-minecraft-ui-light dark:bg-minecraft-ui-dark pixel-corners p-3">
-        <h2 class="text-2xl font-bold mb-4">Account Details</h2>
+<main class="flex items-center justify-center max-w-6xl mx-auto w-[calc(100vw-2rem)] pt-2 pb-28">
+    <div class="bg-minecraft-ui-light w-full dark:bg-minecraft-ui-dark pixel-corners p-3">
+        <h1 class="text-2xl font-bold mb-4">Edit Account Details</h1>
           <div class="mb-4">
-            <input type="text" id="username" placeholder="Username" bind:value={username} class="border-none pixel-corners p-2 w-full" />
-            <textarea name="description" id="description" rows="3" placeholder="Description" bind:value={description} class="border-none pixel-corners p-2 w-full" />
+            <label for="username">
+                Username
+            </label>
+            <input type="text" id="username" placeholder="Username (Max 16 Chars)" maxlength="16" bind:value={username} 
+            class="border-none pixel-corners p-2 max-w-xl w-full block mb-2" />
+            <label for="tagline">
+                Profile Tagline
+            </label>
+            <textarea name="tagline" maxlength="64" id="tagline" rows="2" placeholder="Tell us something about yourself..." bind:value={tagline} 
+            class="border-none pixel-corners p-2 max-w-xl w-full block mb-2" />
         </div>    
-          <button type="button" class="bg-blue-500 text-white py-2 px-4 rounded-md" on:click={saveChanges}>Save</button>
+        <h2>Social Links</h2>
+        <div class="max-w-xl w-full flex gap-2">
+            <input type="text" id="link0_title" placeholder="Name" maxlength="12" bind:value={links[0].name}
+            class="border-none pixel-corners p-2 min-w-[90px] w-[30%] mb-2">
+            <input type="text" id="link0_url" placeholder="URL" maxlength="256" bind:value={links[0].url}
+            class="border-none pixel-corners p-2 flex-grow min-w-[90px] mb-2" >
+        </div>
+        <div class="max-w-xl w-full flex gap-2">
+            <input type="text" id="link0_title" placeholder="Name" maxlength="12" bind:value={links[1].name}
+            class="border-none pixel-corners p-2 min-w-[90px] w-[30%] mb-2">
+            <input type="text" id="link0_url" placeholder="URL" maxlength="256" bind:value={links[1].url}
+            class="border-none pixel-corners p-2 flex-grow min-w-[90px] mb-2" >
+        </div>
+
+          <button type="button" class="bg-create-blue text-white py-2 px-4 pixel-corners" on:click={saveChanges}>Save</button>
       </div>    
 </main>
