ea89e3129db9b1fca02d8eb6f52a28ed4be5a989
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-08 18:07:34 +0100

    fix schematic cards on browse page

--- frontend/src/routes/browse/+page.svelte
+++ frontend/src/routes/browse/+page.svelte
@@ -17,7 +17,7 @@ w-[calc(100vw-2rem)] justify-between items-left pixel-corners"
   <h2 class="text-2xl text-white/500 p-1">Browse Schematics</h2>
   <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mx-auto items-left">
     {#each schematics as schematic}
-      <SchematicCard {...schematic} />
+      <SchematicCard {schematic} />
     {/each}
   </div>
 </main>
