db5fcb27d4be701bdb08b239ed8d124cbbc7d77d
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-08 13:45:00 +0100

    more requests

--- frontend/src/lib/requests.ts
+++ frontend/src/lib/requests.ts
@@ -3,14 +3,25 @@ import type { paths } from '$lib/openapiSchema'
 
 const apity = Apity.for<paths>()
 
-// apity.configure({
-//     baseUrl: ''
-// })
+console.log(import.meta.env.BASE_URL)
+
+apity.configure({
+    //I'm not sure what the production api url is going to be
+    baseUrl: import.meta.env.PROD ? "https://api.createschematics.com/" : "http://localhost:3000",
+})
 
 export const getSchematics = apity.path('/api/v1/schematics')
     .method('get')
     .create()
 
 export const getSchematic = apity.path('/api/v1/schematics/{id}')
-.method('get')
-.create()
+    .method('get')
+    .create()
+
+export const getSchematicTags = apity.path('/api/v1/schematics/{id}/tags')
+    .method('get')
+    .create()
+
+export const getSchematicComments = apity.path('/api/v1/schematics/{id}/comments')
+    .method('get')
+    .create()
