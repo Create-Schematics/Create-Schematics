474249a1728ebae47a46a7563feb31e27a325676
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-08 13:45:27 +0100

    remove obsolete types

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,3 +1,5 @@
+import type { components } from "./openapiSchema";
+
 export interface User {
     id: string;
     username: string;
@@ -9,32 +11,6 @@ export interface User {
     }[];
     totalDownloads: number;
     description: string;
-}
-
-export interface Comment {
-    id: string;
-    author: string;
-    text: string;
-}
-
-export interface Schematic {
-    id: string;
-    tags: string[];
-    uploadDate: Date;
-    title: string;
-    images: string[];
-    downloads: number;
-    likes: number;
-    dislikes: number;
-    views: number;
-    author: string;
-}
-
-export interface SchematicDetails extends Schematic {
-    description: string;
-    comments: Comment[];
-    mods: string[];
-    file: string;
 }
 
 export interface Collection {
