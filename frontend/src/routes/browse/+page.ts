29c80ab7caf1295d76fb3558579c126314c495b3
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-08 15:39:27 +0100

    implement pages to the browse query

--- frontend/src/routes/browse/+page.ts
+++ frontend/src/routes/browse/+page.ts
@@ -4,10 +4,12 @@ import { error } from '@sveltejs/kit'
 
 export const load = async ({url}) => {
     const q = url.searchParams.get('q')
+    const page = url.searchParams.get('page')
     const schematics = await getSchematics({
         query: {
             term: q,
-            limit: 50
+            limit: 50,
+            offset: page ? parseInt(page) * 50 : 0
         }
     }).result
 
