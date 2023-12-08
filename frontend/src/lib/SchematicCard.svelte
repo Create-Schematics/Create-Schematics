4f811e1ca3d127c8e7c7b52d89add707a0f34dc9
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-08 18:05:57 +0100

    update schematic card

--- frontend/src/lib/SchematicCard.svelte
+++ frontend/src/lib/SchematicCard.svelte
@@ -1,56 +1,41 @@
 <script lang="ts">
   import type { Schematic } from "$lib/types";
   import { abbreviateNumber } from "$lib/utils";
+  import type { components } from "./openapiSchema";
+  import { getSchematicTags } from "./requests";
+  export let schematic: components["schemas"]["Schematic"];
 
   let isUserPage = false;
   if (typeof window !== "undefined") {
     isUserPage = window.location.href.includes("user");
   }
-  export const { tags, uploadDate, title, author, downloads, id }: Schematic = {
-    tags: [
-      "farm",
-      "Above & Beyond",
-      "trains",
-      "equipment",
-      "steampunk",
-      "novel",
-    ],
-    uploadDate: new Date(1701111471000),
-    title: "Very cool schematic",
-    images: ["https://picsum.photos/500/800"],
-    downloads: 603,
-    likes: 60,
-    dislikes: 40,
-    views: 894,
-    author: "Szedann",
-    id: "1",
-  };
-  const slicedTags = tags.slice(0, 3);
 </script>
 
 <a
-  href={`/schematic/${id}`}
+  href={`/schematic/${schematic.schematic_id}`}
   class="bg-white dark:bg-black/50 mx-auto px-4 py-3 pixel-corners w-full no-default-link"
 >
-  <h1 class="text-xl font-bold">{title}</h1>
+  <h1 class="text-xl font-bold">{schematic.schematic_name}</h1>
   <div class="text-xs text-opacity-50 w-full">
     {#if !isUserPage}
       <p class="inline">
-        by <a href={`/user/${author}`} class="underline">{author}</a>
+        by <a href={`/user/${schematic.author}`} class="underline"
+          >{schematic.author}</a
+        >
       </p>
       <p class="inline px-2">|</p>
     {/if}
-    <p class="inline"><b>⭳</b> {abbreviateNumber(downloads)}</p>
+    <p class="inline"><b>⭳</b> {abbreviateNumber(schematic.downloads)}</p>
     <p class="inline px-2">|</p>
-    <p class="inline text-right">{uploadDate.toLocaleString("en-US")} UTC</p>
+    <!-- <p class="inline text-right">{schematic.toLocaleString("en-US")} UTC</p> -->
     <img
-      src="https://picsum.photos/500/800"
-      alt=""
+      src={schematic.images[0]}
+      alt={schematic.schematic_name}
       class="object-cover pixel-corners w-full h-full overflow-hidden aspect-video my-2"
     />
   </div>
   <ul class="flex flex-wrap gap-2">
-    {#each slicedTags as tag}
+    {#each schematic.tags as tag}
       <li
         class="text-xs bg-create-blue/50 hover:bg-create-blue/80 px-1 text-opacity-50"
       >
