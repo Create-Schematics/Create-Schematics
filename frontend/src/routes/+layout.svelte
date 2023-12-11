f762442e11423b0fae53f73158accaeebffacf6c
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-11 13:17:01 +0100

    update layout for new api lib

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -73,7 +73,7 @@
           {/if}
         </div>
       </button>
-      {#if user.ok}
+      {#if user}
         <a
           href="/upload"
           class="bg-create-blue/80 no-default-link
@@ -83,10 +83,10 @@
           +
         </a>
         <a
-          href={`/user/${data.user.data.username}`}
+          href={`/user/${user.username}`}
           class="w-10 h-10 bg-white/50 overflow-hidden pixel-corners"
         >
-          <img src={user.data.avatar} alt="avatar" />
+          <img src={user.avatar} alt="avatar" />
         </a>
       {:else}
         <a
