77536e93dc3220083e219832db40d2fb39276361
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-11 13:17:23 +0100

    update browse page for new api lib

--- frontend/src/routes/browse/+page.ts
+++ frontend/src/routes/browse/+page.ts
@@ -1,18 +1,22 @@
-import { getSchematics } from '$lib/requests.js'
+import { GET } from '$lib/requests.js'
 import { error } from '@sveltejs/kit'
 
 
 export const load = async ({url}) => {
     const q = url.searchParams.get('q')
     const page = url.searchParams.get('page')
-    const schematics = await getSchematics({
-        query: {
-            term: q,
-            limit: 50,
-            offset: page ? parseInt(page) * 50 : 0
+    const schematics = await GET("/api/v1/schematics", {
+        params: {
+            query: {
+                query: {
+                    term: q,
+                    limit: 50,
+                    offset: page ? parseInt(page) * 50 : 0
+                }
+            }
         }
-    }).result
+    })
 
-    if(!schematics.ok) throw error(500)
+    if(!schematics.data) throw error(500)
     return {schematics: schematics.data}
 }
