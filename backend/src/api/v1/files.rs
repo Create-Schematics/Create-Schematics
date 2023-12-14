b2781cf6bc9f38f284db9a397b0be04af53f1358
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-12-14 18:29:51 +0000

    Migrated file endpoints

--- backend/src/api/v1/files.rs
+++ backend/src/api/v1/files.rs
@@ -1,11 +1,9 @@
 use std::path::PathBuf;
 
-use axum::routing::get;
-use axum::{Router, Json};
-use axum::extract::{Path, State};
-use axum::body::Bytes;
-use axum_typed_multipart::{FieldData, TypedMultipart, TryFromMultipart};
-use utoipa::ToSchema;
+use poem::web::Data;
+use poem_openapi::param::Path;
+use poem_openapi::payload::Json;
+use poem_openapi_derive::{Object, Multipart, OpenApi};
 use uuid::Uuid;
 
 use crate::error::ApiError;
@@ -15,192 +13,166 @@ use crate::storage::{UPLOAD_PATH, SCHEMATIC_PATH};
 use crate::response::ApiResult;
 use crate::api::ApiContext;
 
-pub (in crate::api::v1) fn configure() -> Router<ApiContext> {
-    Router::new()
-        .route(
-            "/schematics/:id/files", 
-            get(get_files_from_schematic)
-            .post(upload_file_to_schematic)
-            .delete(remove_file_from_schematic)
-        )
-}
+const MAX_FILE_SIZE: u64 = 256 * 1024; // 256kb
 
-#[derive(Serialize, Debug, ToSchema)]
-pub (in crate::api) struct Files {
-    #[schema(min_items=1)]
+pub (in crate::api::v1) struct FileApi;
+
+#[derive(Serialize, Debug, Object)]
+pub (in crate::api::v1) struct Files {
+    #[oai(validator(min_items=1))]
     pub files: Vec<String>
 }
 
-#[derive(TryFromMultipart, Debug, ToSchema)]
+#[derive(Multipart, Debug)]
 pub (in crate::api) struct UploadFile {
-    #[form_data(limit = "2KiB")]
-    #[schema(value_type=String, format=Binary)]
-    pub file: FieldData<Bytes>
+    pub file: UploadFile
 }
 
-#[derive(TryFromMultipart, Debug, ToSchema)]
+#[derive(Multipart, Debug)]
 pub (in crate::api) struct DeleteFile {
-    #[schema(example="my_schematic.nbt")]
+
     pub file_name: String
 }
 
-#[utoipa::path(
-    get,
-    path = "/schematics/{schematic_id}/files",
-    context_path = "/api/v1",
-    tag = "v1",
-    params(
-        ("schematic_id" = Uuid, Path, description = "The id of the schematic to fetch files from"),
-    ),
-    responses(
-        (status = 200, description = "Successfully retrieved the images", body = Files, content_type = "application/json"),
-        (status = 500, description = "An internal server error occurred")
-    ),
-    security(())
-)]
-async fn get_files_from_schematic(
-    Path(schematic_id): Path<Uuid>,
-    State(ctx): State<ApiContext>
-) -> ApiResult<Json<Files>> {
-    sqlx::query_as!(
-        Files,
-        r#"
-        select files
-        from schematics
-        where schematic_id = $1
-        "#,
-        schematic_id
-    )
-    .fetch_optional(&ctx.pool)
-    .await?
-    .ok_or(ApiError::NotFound)
-    .map(Json)
-}
+#[OpenApi(prefix_path="/api/v1")]
+impl FileApi {
 
-#[utoipa::path(
-    post,
-    path = "/schematics/{schematic_id}/files",
-    context_path = "/api/v1",
-    tag = "v1",
-    params(
-        ("schematic_id" = Uuid, Path, description = "The id of the schematic to upload a file to")
-    ),
-    request_body(
-        content = UploadFile, description = "The new schematic file", content_type = "multipart/form-data"
-    ),
-    responses(
-        (status = 200, description = "Successfully uploaded the file to the schematic"),
-        (status = 401, description = "You need to be logged in to upload a new file to a schematic"),
-        (status = 403, description = "You do not have permission to add a file to this schematic"),
-        (status = 404, description = "A schematic with that id was not found"),
-        (status = 409, description = "This schematic already has a file with that name"),
-        (status = 500, description = "An internal server error occurred")
-    ),
-    security(("session_cookie" = []))
-)]
-async fn upload_file_to_schematic(
-    Path(schematic_id): Path<Uuid>,
-    State(ctx): State<ApiContext>,
-    user: User,
-    TypedMultipart(form): TypedMultipart<UploadFile>
-) -> ApiResult<()> {
-    let file_name = form.file.metadata.file_name.ok_or(ApiError::BadRequest)?;
-    let mut transaction = ctx.pool.begin().await?;
-    
-    Schematic::check_user_permissions(user, &schematic_id, Permissions::MODERATE_POSTS, &mut *transaction).await?;
-    let sanitized = sanitize_filename::sanitize(file_name);
-
-    sqlx::query!(
-        r#"
-        update schematics
-            set 
-                files = array_append(files, $1)
-            where 
-                schematic_id = $2
-        "#,
-        sanitized,
-        schematic_id
-    )
-    .execute(&mut *transaction)
-    .await?;
-
-    let mut path = PathBuf::from(UPLOAD_PATH);
-    path.push(schematic_id.to_string());
-    path.push(SCHEMATIC_PATH);
-
-    let file = path.join(sanitized);
-    
-    if file.exists() {
-        return Err(ApiError::Conflict);
+    /// Fetches the name of all uploaded schematic files on a given schematic
+    /// 
+    /// Note this does not return the schematic files themselves, they can be 
+    /// retrieved from the static file endpoint like so filling in the schematic
+    /// id for the given schematic and file_name for one of the values returned
+    /// here `GET /upload/schematics/{schematic_id}/files/{file_name}.nbt`
+    /// 
+    #[oai(path = "/schematics/:id/files", method = "get")]
+    async fn get_files_from_schematic(
+        &self,
+        Data(ctx): Data<&ApiContext>,    
+        Path(schematic_id): Path<Uuid>,
+    ) -> ApiResult<Json<Files>> {
+        sqlx::query_as!(
+            Files,
+            r#"
+            select files
+            from schematics
+            where schematic_id = $1
+            "#,
+            schematic_id
+        )
+        .fetch_optional(&ctx.pool)
+        .await?
+        .ok_or(ApiError::NotFound)
+        .map(Json)
     }
 
-    tokio::fs::write(file, form.file.contents)
-        .await
-        .map_err(anyhow::Error::new)?;
+    /// Uploads a new schematic file to a schematic, use this for schematics
+    /// with multiple variations or parts not for many entirely different 
+    /// schematics. 
+    /// 
+    /// This requires for the current user to be the owner of the given schematic
+    /// and for this file name (after sanitization) to not be used already. If
+    /// there are conflicting file names `422 Unprocessable Entity` will be returned
+    /// with a message explaining this
+    ///  
+    #[oai(path = "/schematics/:id/files", method = "post")]
+    async fn upload_file_to_schematic(
+        &self,
+        Data(ctx): Data<&ApiContext>,    
+        Path(schematic_id): Path<Uuid>,
+        user: User,
+        form: UploadFile
+    ) -> ApiResult<()> {
+        let file_name = form.file.file_name.ok_or(ApiError::BadRequest)?;
+        let sanitized = sanitize_filename::sanitize(file_name);
+        
+        if form.file.contents.len() > MAX_FILE_SIZE || !sanitized.endswith(".nbt") {
+            return Err(ApiError::BadRequest);
+        }
+
+        let mut path = PathBuf::from(UPLOAD_PATH);
+        path.push(schematic_id.to_string());
+        path.push(SCHEMATIC_PATH);
     
-    transaction.commit().await?;
+        let file = path.join(sanitized);
+        
+        if file.exists() {
+            return Err(ApiError::Conflict);
+        }
 
-    Ok(())
-}
-
-#[utoipa::path(
-    delete,
-    path = "/schematics/{schematic_id}/files",
-    context_path = "/api/v1",
-    tag = "v1",
-    params(
-        ("schematic_id" = Uuid, Path, description = "The id of the schematic to remove a file from")
-    ),
-    request_body(
-        content = DeleteImage, description = "The name of the file to remove", content_type = "multipart/form-data"
-    ),
-    responses(
-        (status = 200, description = "Successfully deleted the file from the schematic", body = Images, content_type = "application/json"),
-        (status = 401, description = "You need to be logged in to remove a file from a schematic"),
-        (status = 403, description = "You do not have permission to remove a file from this schematic"),
-        (status = 404, description = "A schematic with that id or a file with that name was not found"),
-        (status = 500, description = "An internal server error occurred")
-    ),
-    security(("session_cookie" = []))
-)]
-async fn remove_file_from_schematic(
-    Path(schematic_id): Path<Uuid>,
-    State(ctx): State<ApiContext>,
-    user: User,
-    TypedMultipart(form): TypedMultipart<DeleteFile>
-) -> ApiResult<Json<Files>> {
-    let mut transaction = ctx.pool.begin().await?;
+        let mut transaction = ctx.pool.begin().await?;
+        Schematic::check_user_permissions(user, &schematic_id, Permissions::MODERATE_POSTS, &mut *transaction).await?;
     
-    Schematic::check_user_permissions(user, &schematic_id, Permissions::MODERATE_POSTS, &mut *transaction).await?;
-    let file_name = sanitize_filename::sanitize(form.file_name);
+        sqlx::query!(
+            r#"
+            update schematics
+                set 
+                    files = array_append(files, $1)
+                where 
+                    schematic_id = $2
+            "#,
+            sanitized,
+            schematic_id
+        )
+        .execute(&mut *transaction)
+        .await?;
+        
+        tokio::fs::write(file, form.file.contents)
+            .await
+            .map_err(anyhow::Error::new)?;
 
-    let files = sqlx::query_as!(
-        Files,
-        r#"
-        update schematics
-            set 
-                files = array_remove(files, $1)
-            where 
-                schematic_id = $2
-                and array_length(files, 1) > 2
-        returning files
-        "#,
-        file_name,
-        schematic_id
-    )
-    .fetch_optional(&mut *transaction)
-    .await?
-    .ok_or(ApiError::NotFound)?;
+        transaction.commit().await?;
+    
+        Ok(())
+    }
 
-    let mut path = PathBuf::from(UPLOAD_PATH);
-    path.push(schematic_id.to_string());
-    path.push(SCHEMATIC_PATH);
-
-    tokio::fs::remove_file(path.join(file_name))
-        .await
-        .map_err(anyhow::Error::new)?;
-
-    transaction.commit().await?;
-
-    Ok(Json(files))
+    /// Removes a schematic file from a schematic, at least one file must be
+    /// present at all times. Requests to remove the last file will result in
+    /// a `400 Bad Request` error
+    /// 
+    /// This requires the current to user to either own the schematic or have
+    /// permissions to moderate schematics
+    /// 
+    #[oai(path = "/schematics/:id/files", method = "delete")]
+    async fn remove_file_from_schematic(
+        &self,
+        Data(ctx): Data<&ApiContext>,   
+        Path(schematic_id): Path<Uuid>,
+        user: User,
+        form: DeleteFile
+    ) -> ApiResult<Json<Files>> {
+        let mut transaction = ctx.pool.begin().await?;
+        
+        Schematic::check_user_permissions(user, &schematic_id, Permissions::MODERATE_POSTS, &mut *transaction).await?;
+        let file_name = sanitize_filename::sanitize(form.file_name);
+    
+        let files = sqlx::query_as!(
+            Files,
+            r#"
+            update schematics
+                set 
+                    files = array_remove(files, $1)
+                where 
+                    schematic_id = $2
+                    and array_length(files, 1) > 2
+            returning files
+            "#,
+            file_name,
+            schematic_id
+        )
+        .fetch_optional(&mut *transaction)
+        .await?
+        .ok_or(ApiError::NotFound)?;
+    
+        let mut path = PathBuf::from(UPLOAD_PATH);
+        path.push(schematic_id.to_string());
+        path.push(SCHEMATIC_PATH);
+    
+        tokio::fs::remove_file(path.join(file_name))
+            .await
+            .map_err(anyhow::Error::new)?;
+    
+        transaction.commit().await?;
+    
+        Ok(Json(files))
+    }   
 }
