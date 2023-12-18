5e2009ca51c44d08a7c3f2e0d8fefa305ee563f7
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-12-18 17:05:47 +0000

    Replaced error handling to derive ApiResponse

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -1,10 +1,7 @@
-use poem::web::Json;
-use poem::{IntoResponse, Response};
-use poem::http::{StatusCode, HeaderValue};
-use poem::http::header::WWW_AUTHENTICATE;
-use poem::http::HeaderMap;
+use poem_openapi::ApiResponse;
+use poem_openapi::payload::Json;
+use poem_openapi_derive::Object;
 use sqlx::error::DatabaseError;
-use std::borrow::Cow;
 use std::collections::HashMap;
 
 /// A common error type that can be used throughout the API.
@@ -14,16 +11,23 @@ use std::collections::HashMap;
 /// For convenience, this represents both API errors as well as internal recoverable errors,
 /// and maps them to appropriate status codes along with at least a minimally useful error
 /// message in a plain text body, or a JSON body in the case of `UnprocessableEntity`.
-#[derive(thiserror::Error, Debug)]
+/// 
+/// This error handling solution should probably be replaced at some point, this is a 
+/// re-implemenation of the existing errors to work with poem 
+/// 
+/// https://github.com/Create-Schematics/Create-Schematics/blob/50adde233e45a66ede0bf1d3013bf8e2c0a81623/backend/src/error.rs
+///
+#[derive(Debug, ApiResponse)]
 pub enum ApiError {
+
     // Return '400 Bad Request'
-    #[error("bad request")]
+    #[oai(status = 400)]
     BadRequest,
 
     // Return '401' Unauthorized, this is typically only
     // raised by middleware when a token is missing, expired,
     // malformed or otherwise invalid
-    #[error("authentication is required, please provide a valid bearer token")]
+    #[oai(status = 401)]
     Unauthorized,
 
     // Return '403 Forbidden, for when the user's identity
@@ -31,65 +35,38 @@ pub enum ApiError {
     // they are attempting to access or do not have permissions
     // within the project to perform the action within the
     // project
-    #[error("insufficient permissions to perform this action")]
+    #[oai(status = 403)]
     Forbidden,
 
     // Return '404' Not Found
-    #[error("resource not found")]
+    #[oai(status = 404)]
     NotFound,
-    
-    /// Return '409 Conflict'
-    #[error("this resource already exists")]
-    Conflict,
 
     /// Return `422 Unprocessable Entity`
     ///
     /// This also serializes the `errors` map provided to JSON
     ///
-    #[error("error in the request body")]
-    UnprocessableEntity {
-        errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
-    },
+    #[oai(status = 422)]
+    UnprocessableEntity(Json<EntityErrors>),
 
-    /// Automatically return `500 Internal Server Error` on a `sqlx::Error`.
-    ///
-    /// Via the generated `From<sqlx::Error> for Error` impl,
-    /// this allows using `?` on database calls in handler functions without a manual mapping step.
-    ///
-    /// The actual error message isn't returned to the client for security reasons.
-    /// It should be logged instead.
-    ///
-    /// Note that this could also contain database constraint errors, which should usually
-    /// be transformed into client errors (e.g. `422 Unprocessable Entity` or `409 Conflict`).
-    /// See `ResultExt` below for a convenient way to do this.
-    #[error("an error occurred with the database")]
-    Sqlx(#[from] sqlx::Error),
-
-    /// Automatically return `500 Internal Server Error` on a `redis::RedisError`.
-    ///
-    /// Via the generated `From<redis::RedisError> for Error` impl,
-    /// this allows using `?` on database calls in handler functions without a manual mapping step.
-    ///
-    /// The actual error message isn't returned to the client for security reasons.
-    /// It should be logged instead.
+    /// Return `500 Internal Server Error`
     /// 
-    #[error("an error occurred with the database")]
-    Redis(#[from] redis::RedisError),
+    /// This should generally be called implicity by another
+    /// error see implementation bellow
+    /// 
+    #[oai(status = 500)]
+    InternalServerError,
+}
 
-    /// Return `500 Internal Server Error` on a `anyhow::Error`.
-    ///
-    /// `anyhow::Error` is used in a few places to capture context and backtraces
-    /// on unrecoverable (but technically non-fatal) errors which could be highly useful for
-    /// debugging. We use it a lot in our code for background tasks or making API calls
-    /// to external services so we can use `.context()` to refine the logged error.
-    ///
-    /// Via the generated `From<anyhow::Error> for Error` impl, this allows the
-    /// use of `?` in handler functions to automatically convert `anyhow::Error` into a response.
-    ///
-    /// Like with `Error::Sqlx`, the actual error message is not returned to the client
-    /// for security reasons.
-    #[error("an internal server error occurred")]
-    Anyhow(#[from] anyhow::Error)
+#[derive(Debug, Object, Serialize)]
+pub struct EntityErrors {
+    /// Structure to return unprocessable data in with the erroneous field
+    /// as the key and the reason it cannot be handled in the value.
+    /// 
+    /// Ideally we would use a Cow<'static, str> here to avoid unnessasery
+    /// cloning of the strings but does not implement ParseFromJSON
+    /// 
+    errors: HashMap<String, Vec<String>>,
 }
 
 impl ApiError {
@@ -97,78 +74,80 @@ impl ApiError {
     ///
     /// Multiple for the same key are collected into a list for that key.
     ///
-    pub fn unprocessable_entity<K, V>(errors: impl IntoIterator<Item = (K, V)>) -> Self
+    pub fn unprocessable_entity<K, V>(values: impl IntoIterator<Item = (K, V)>) -> Self
     where
-        K: Into<Cow<'static, str>>,
-        V: Into<Cow<'static, str>>,
+        K: Into<String>,
+        V: Into<String>,
     {
-        let mut error_map = HashMap::new();
+        let mut errors: HashMap<String, Vec<String>> = HashMap::new();
 
-        for (key, val) in errors {
-            error_map
+        for (key, val) in values {
+            errors
                 .entry(key.into())
                 .or_insert_with(Vec::new)
                 .push(val.into());
         }
 
-        Self::UnprocessableEntity { errors: error_map }
-    }
-
-    fn status_code(&self) -> StatusCode {
-        match self {
-            Self::BadRequest => StatusCode::BAD_REQUEST,
-            Self::Unauthorized => StatusCode::UNAUTHORIZED,
-            Self::Forbidden => StatusCode::FORBIDDEN,
-            Self::Conflict => StatusCode::CONFLICT,
-            Self::NotFound => StatusCode::NOT_FOUND,
-            Self::UnprocessableEntity { .. } => StatusCode::UNPROCESSABLE_ENTITY,
-            _ => StatusCode::INTERNAL_SERVER_ERROR, 
-        }
+        Self::UnprocessableEntity(Json(EntityErrors { errors }))
     }
 }
 
-impl IntoResponse for ApiError {
-    fn into_response(self) -> Response {
-        match self {
-           Self::UnprocessableEntity { errors } => {
-                #[derive(serde::Serialize)]
-                struct Errors {
-                    errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
-                }
-                    
-                return (StatusCode::UNPROCESSABLE_ENTITY, Json(Errors { errors })).into_response();
-            },
+/// Return `500 Internal Server Error` on a `anyhow::Error`.
+///
+/// `anyhow::Error` is used in a few places to capture context and backtraces
+/// on unrecoverable (but technically non-fatal) errors which could be highly useful for
+/// debugging. We use it a lot in our code for background tasks or making API calls
+/// to external services so we can use `.context()` to refine the logged error.
+///
+/// Via the generated `From<anyhow::Error> for Error` impl, this allows the
+/// use of `?` in handler functions to automatically convert `anyhow::Error` into a response.
+///
+/// Like with `Error::Sqlx`, the actual error message is not returned to the client
+/// for security reasons.
+/// 
+impl From<anyhow::Error> for ApiError {
+    fn from(error: anyhow::Error) -> Self {
+        tracing::error!("Generic error: {:?}", error);
 
-            Self::Unauthorized => {
-                let mut headers = HeaderMap::new();
-                
-                // Include the `WWW-Authenticate` challenge required in the specification
-                // for the `401 Unauthorized` response code:
-                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
-                //
-                headers.append(WWW_AUTHENTICATE, HeaderValue::from_static("session"));
-
-                return (headers, self.status_code()).into_response();
-            }
-
-            Self::Sqlx(ref e) => {
-                tracing::error!("SQLx error: {:?}", e);
-            }
-
-            Self::Redis(ref e) => {
-                tracing::error!("Redis error: {:?}", e);
-            }
-
-            Self::Anyhow(ref e) => {
-                tracing::error!("Generic error: {:?}", e);
-            }
-
-            _ => ()
-        };
-
-        (self.status_code(), self.to_string()).into_response()
+        ApiError::InternalServerError
     }
 }
+
+/// Automatically return `500 Internal Server Error` on a `sqlx::Error`.
+///
+/// Via the generated `From<sqlx::Error> for Error` impl,
+/// this allows using `?` on database calls in handler functions without a manual mapping step.
+///
+/// The actual error message isn't returned to the client for security reasons.
+/// It should be logged instead.
+///
+/// Note that this could also contain database constraint errors, which should usually
+/// be transformed into client errors (e.g. `422 Unprocessable Entity` or `409 Conflict`).
+/// See `ResultExt` below for a convenient way to do this.
+impl From<sqlx::error::Error> for ApiError {
+    fn from(error: sqlx::error::Error) -> Self {
+        tracing::error!("Database error: {:?}", error);
+
+        ApiError::InternalServerError
+    }
+}
+
+/// Automatically return `500 Internal Server Error` on a `redis::RedisError`.
+///
+/// Via the generated `From<redis::RedisError> for Error` impl,
+/// this allows using `?` on database calls in handler functions without a manual mapping step.
+///
+/// The actual error message isn't returned to the client for security reasons.
+/// It should be logged instead.
+/// 
+impl From<redis::RedisError> for ApiError {
+    fn from(error: redis::RedisError) -> Self {
+        tracing::error!("Redis error: {:?}", error);
+
+        ApiError::InternalServerError
+    }
+}
+
 
 /// A little helper trait for more easily converting database constraint errors into API errors.
 ///
@@ -203,20 +182,17 @@ pub trait ResultExt<T> {
     ) -> Result<T, ApiError>;
 }
 
-impl<T, E> ResultExt<T> for Result<T, E>
-where
-    E: Into<ApiError>,
-{
+impl<T> ResultExt<T> for Result<T, sqlx::Error> {
     fn on_constraint(
         self,
         name: &str,
         map_err: impl FnOnce(Box<dyn DatabaseError>) -> ApiError,
     ) -> Result<T, ApiError> {
-        self.map_err(|e| match e.into() {
-            ApiError::Sqlx(sqlx::Error::Database(dbe)) if dbe.constraint() == Some(name) => {
+        self.map_err(|e| match e {
+            sqlx::Error::Database(dbe) if dbe.constraint() == Some(name) => {
                 map_err(dbe)
             }
-            e => e,
+            e => e.into(),
         })
     }
 }
