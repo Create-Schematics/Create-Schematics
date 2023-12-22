a47d039129c486f537b625e00215861174daf2a3
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-22 17:53:44 +0100

    make schematic requests run in parallel

--- frontend/src/routes/schematic/[id]/+page.ts
+++ frontend/src/routes/schematic/[id]/+page.ts
@@ -2,29 +2,30 @@ import { GET } from '$lib/requests.js'
 import { error } from '@sveltejs/kit'
 
 export const load = async ({ params }) => {
-
-    const schematic = await GET("/api/v1/schematics/{schematic_id}", {
-        params: {
-            path: {
-                schematic_id: params.id
+    const [schematic, tags, comments] = await Promise.all([
+        GET("/v1/schematics/{schematic_id}", {
+            params: {
+                path: {
+                    schematic_id: params.id
+                }
             }
-        }
-    })
-    const tags = await GET("/api/v1/schematics/{schematic_id}/tags", {
-        params: {
-            path: {
-                schematic_id: params.id
+        }),
+        GET("/v1/schematics/{schematic_id}/comments", {
+            params: {
+                path: {
+                    schematic_id: params.id
+                },
+                query: {}
             }
-        }
-    })
-    const comments = await GET("/api/v1/schematics/{schematic_id}/comments", {
-        params: {
-            path: {
-                schematic_id: params.id
-            },
-            query: {query: {}}
-        }
-    })
+        }),
+        GET("/v1/schematics/{schematic_id}/tags", {
+            params: {
+                path: {
+                    schematic_id: params.id
+                }
+            }
+        })
+    ])
     if(!schematic.data) throw error(404, "Schematic not found")
     if(!tags.data) throw error(500)
     if(!comments.data) throw error(500)
