be9b45a3c9bc77850d5f2e58968b6fdc0f11a9b4
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-12-22 21:47:56 +0000

    Implemented unique suffix handling incase of duplicate usernames

--- backend/src/api/auth/mod.rs
+++ backend/src/api/auth/mod.rs
@@ -8,6 +8,7 @@ use poem::web::Data;
 use poem_openapi::OpenApi;
 use poem_openapi::param::{Path, Query};
 use poem_openapi_derive::ApiResponse;
+use rand::Rng;
 use reqwest::header;
 use uuid::Uuid;
 
@@ -99,29 +100,31 @@ impl AuthApi {
         let mut transaction = ctx.pool.begin().await?;
         let oauth_user = oauth_client.extract_user(&response)?;
 
-        let user_id = provider.get_or_create_user(oauth_user, &mut transaction).await?;
-        transaction.commit().await?;
-        
+        let user_id = oauth_user.get_or_create_user(provider, &mut transaction).await?;
         let session = UserSession::new_for_user(user_id);
-
+        
         session.save(&ctx.redis_pool).await?;
         cookies.add(session.into_cookie());
+        
+        transaction.commit().await?;
 
         Ok(RedirectResponse::to("/"))
     }
 }
 
-impl OauthProvider {
+impl OauthUser {
+    pub const MAX_ATTEMPTS: usize = 5;
+
     async fn get_or_create_user(
-        &self,
-        user: OauthUser,
+        self,
+        provider: OauthProvider,
         transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
     ) -> ApiResult<Uuid> {
-        let provider = self.to_string();
+        let provider = provider.to_string();
 
         let user_meta = sqlx::query!(
             r#"select user_id from users where oauth_provider = $1 and oauth_id = $2"#,
-            provider, user.oauth_id
+            provider, self.oauth_id
         )
         .fetch_optional(&mut **transaction)
         .await?;
@@ -130,24 +133,46 @@ impl OauthProvider {
             return Ok(user_meta.user_id)
         } 
 
-        let username = user.display_name.unwrap_or(user.username);  
+        let mut username = self.username.clone();
+        let mut attempts = 0;
+
+        
+        while Self::is_username_used(&username, transaction).await? {
+            // If the username is already used add a random value to the end of the username until it 
+            // is unqiue for example Rabbitminers -> Rabbitminers123
+            let suffix = rand::thread_rng().gen_range(10..=999);
+            username = format!("{username}{suffix}");
+            
+            attempts += 1;
+            
+            // Check if attempts are exceeded here to prevent hitting the database again needlessly
+            if attempts > Self::MAX_ATTEMPTS {
+                return Err(ApiError::InternalServerError)
+            }
+        }
+        
+        // If the oauth provider doesnt return a display name use the username before any suffixes are 
+        // added to it as display names do not need to be unique unlike usernames.
+        let display_name = self.display_name.unwrap_or(self.username);
 
         let user_id = sqlx::query!(
             r#"
             insert into users (
-                username, email, avatar,
-                oauth_provider, oauth_id
+                username, displayname, 
+                email, avatar, oauth_id,
+                oauth_provider
             )
             values (
-                $1, $2, $3, $4, $5
+                $1, $2, $3, $4, $5, $6
             )
             returning user_id
             "#,
             username,
-            user.email,
-            user.avatar_url,
-            provider,
-            user.oauth_id,
+            display_name,
+            self.email,
+            self.avatar_url,
+            self.oauth_id,
+            provider
         )
         .fetch_one(&mut **transaction)
         .await
@@ -157,5 +182,20 @@ impl OauthProvider {
         .user_id;
 
         Ok(user_id)
+    }
+
+    async fn is_username_used(
+        username: &str,
+        transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+    ) -> ApiResult<bool> {
+        let username_used = sqlx::query!(
+            r#"select user_id from users where username = $1"#,
+            username
+        )
+        .fetch_optional(&mut **transaction)
+        .await?
+        .is_some();
+
+        Ok(username_used)
     }
 }
