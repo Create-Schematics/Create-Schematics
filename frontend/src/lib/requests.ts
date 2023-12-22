1c556d51d2f7a2626143fe3311c3e800a6fdbf75
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-22 17:08:02 +0100

    expose the api base url

--- frontend/src/lib/requests.ts
+++ frontend/src/lib/requests.ts
@@ -1,5 +1,7 @@
 import type { paths } from '$lib/openapiSchema'
 import createClient from 'openapi-fetch';
 
+export const apiBaseUrl = import.meta.env.PROD ? "https://createschematics.com/" : "http://localhost:3000"
 
-export const { GET, POST } = createClient<paths>({ baseUrl: import.meta.env.PROD ? "https://createschematics.com/" : "http://localhost:3000" });
+
+export const { GET, POST } = createClient<paths>({ baseUrl: apiBaseUrl });
