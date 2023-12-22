d89ca00454cf087f8bc4d7b73a10722681f4bc1b
Szedann <65607498+Szedann@users.noreply.github.com>
2023-12-22 22:57:58 +0100

    rename current user from layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
-  import Header from '$lib/components/layout/Header.svelte';
-  import Footer from '$lib/components/layout/Footer.svelte';
-  
+  import Header from "$lib/components/layout/Header.svelte";
+  import Footer from "$lib/components/layout/Footer.svelte";
+
   import type { PageData } from "./$types";
   import "../app.css";
-  
-	import { ModeWatcher, toggleMode, mode } from 'mode-watcher';
+
+  import { ModeWatcher } from "mode-watcher";
 
   export let data: PageData;
-  const user = data.user;
-
+  const user = data.currentUser;
 </script>
 
 <ModeWatcher />
 
 <body
-  class=" bg-create-blue/40 dark:bg-gray-800 dark:text-white absolute w-full flex flex-col gap-3 font-pixel"
+  class=" bg-create-blue/40 dark:bg-gray-800 dark:text-white absolute w-full flex flex-col gap-3 font-pixel min-h-screen"
 >
-  <Header {user}/>
-  
-  <main class="max-w-6xl m-auto p-4">
+  <Header {user} />
+
+  <main class="max-w-6xl m-auto p-4 min-h-screen">
     <slot />
   </main>
-  
-  <Footer/>
 
+  <Footer />
 </body>
