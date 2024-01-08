e80082459314e62f46b8d03fd11e49eeccec6d5b
Szedann <65607498+Szedann@users.noreply.github.com>
2024-01-08 14:51:03 +0100

    const instead of let

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,16 +1,19 @@
-export const abbreviateNumber = (num:number) => {
-    if (num < 1000) {
-        return num;
+export const abbreviateNumber = (num: number) => {
+  if (num < 1000) {
+    return num;
+  }
+  const si = [
+    { v: 1e3, s: "K" },
+    { v: 1e6, s: "M" },
+  ];
+  let index;
+  for (index = si.length - 1; index > 0; index--) {
+    if (num >= si[index].v) {
+      break;
     }
-    let si = [
-      {v: 1E3, s: "K"},
-      {v: 1E6, s: "M"},
-      ];
-    let index;
-    for (index = si.length - 1; index > 0; index--) {
-        if (num >= si[index].v) {
-            break;
-        }
-    }
-    return (num / si[index].v).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + si[index].s;
+  }
+  return (
+    (num / si[index].v).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") +
+    si[index].s
+  );
 };
