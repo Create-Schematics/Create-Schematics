5d6e530322d597af92875fdc183e41952c9b7365
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-01-09 19:13:19 +0000

    Updated migrations for file requirements

--- backend/migrations/20231111114127_init.sql
+++ backend/migrations/20231111114127_init.sql
@@ -38,13 +38,58 @@ create table schematics
     create_version_id serial      not null     references create_versions (create_version_id),
     author            uuid        not null     references users (user_id),
     images            text[]      not null,
-    files             text[]      not null,
     downloads         integer     not null     default 0,
     created_at        timestamptz not null     default now(),
     updated_at        timestamptz
 );
 
 select trigger_updated_at('schematics');
+
+create table files 
+(
+    file_id      uuid        primary key default uuid_generate_v1mc(),
+    file_name    text        not null, 
+    schematic_id uuid        not null    references schematics (schematic_id) on delete cascade,
+    requirements text[]      not null,     
+    created_at   timestamptz not null    default now(),
+    updated_at   timestamptz,
+    unique       (file_name, schematic_id)
+);
+
+select trigger_updated_at('files');
+
+create table mods
+(
+    mod_id          uuid         primary key default uuid_generate_v1mc(),
+    mod_slug        text         not null    unique,
+    mod_name        text,
+    curseforge_slug int                      unique,
+    modrinth_slug   text                     unique,
+    created_at      timestamptz  not null    default now(),
+    updated_at      timestamptz
+);
+
+select trigger_updated_at('mods');
+
+create table mod_proposals
+(
+    proposal_id     uuid         primary key default uuid_generate_v1mc(),
+    user_id         uuid         not null    references users (user_id)    on delete cascade,
+    mod_id          uuid         not null    references mods (mod_id)      on delete cascade,
+    mod_slug        text,
+    mod_name        text,
+    curseforge_slug int,
+    modrinth_slug   text,
+    created_at      timestamptz  not null    default now(),
+    unique (user_id, mod_id)
+);
+
+create table mod_dependencies
+(
+    file_id uuid not null references files (file_id) on delete cascade,
+    mod_id  uuid not null references mods  (mod_id)  on delete cascade,
+    primary key (file_id, mod_id)
+);
 
 create table comments
 (
@@ -71,26 +116,6 @@ create table schematic_likes
 );
 
 select trigger_updated_at('schematic_likes');
-
-create table mods
-(
-    mod_id          text primary key,
-    mod_name        text unique collate "case_insensitive",
-    curseforge_slug int  unique,
-    modrinth_slug   text unique,
-    created_at      timestamptz  not null default now(),
-    updated_at      timestamptz
-);
-
-select trigger_updated_at('mods');
-
-create table mod_dependencies
-(
-    schematic_id uuid not null references schematics (schematic_id) on delete cascade,
-    mod_id       text not null references mods       (mod_id)       on delete cascade,
-    -- Ensure one schematic cannot depend on the same mod twice
-    primary key (schematic_id, mod_id)
-);
 
 create table tags
 (
